b6b3cf573b0795d30902ee5ff20b157cc45f221d
wandrey7 <123254136+wandrey7@users.noreply.github.com>
2025-12-03 14:49:40 -0300

    feat(models): add Category and Product models with migrations

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
