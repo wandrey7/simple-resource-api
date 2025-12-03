1c68e55e54e9ee64d0de1b9f66cbf15a12422826
wandrey7 <123254136+wandrey7@users.noreply.github.com>
2025-12-03 16:01:30 -0300

    feat(views): add Category and Product viewsets for API

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
