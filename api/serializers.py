836662cbaf8b12ae4d103afefbb02b785f8bf64a
wandrey7 <123254136+wandrey7@users.noreply.github.com>
2025-12-03 15:51:38 -0300

    feat(serializers): add Category and Product serializers for API

---
+++ api/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Category, Product
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id','name']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(), source='category', write_only=True
+    )
+
+    class Meta:
+        model = Product
+        fields = ['id','name', 'description', 'price', 'category', 'category_id']
